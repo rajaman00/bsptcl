13b29c3f28fb7066316d94bdbb0bff91bf00e1b9
rajaman00 <raj249919@gmail.com>
2024-06-19 12:37:42 +0530

    Initial commit

---
+++ components/test.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+function Test() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary"  onClick={handleShow}>
+        Launch
+      </Button>
+
+      <Alert variant="info" className="d-none d-lg-block">
+        Resize your browser to show the responsive offcanvas toggle.
+      </Alert>
+
+      <Offcanvas show={show} onHide={handleClose} responsive="lg">
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Responsive offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <p className="mb-0">
+            This is content within an <code>.offcanvas-lg</code>.
+          </p>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  );
+}
+
+
+export default Test;
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css'
+import NavigationBar from '../components/NavigationBar';
+import Slider from '../components/Slider';
+import Test from '../components/test';
+import About from '../components/about';
+function App() {
+  return (
+    <div className="app-container"> 
+    <NavigationBar /> {/* navbar contents viz utiliy menu, shortcut menu and main menu on the right and company name and logo in the left */}
+    <Slider />
+    <About/>
+  </div>
+  );
+}
+
+export default App;
